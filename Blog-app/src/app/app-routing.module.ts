5f874a058ebfd91fabfca88f78f74dae9bf608d2
Igor Tsiumrak <tsiumrak@ukr.net>
2023-04-30 16:51:17 -0400

    add routing

--- Blog-app/src/app/app-routing.module.ts
+++ Blog-app/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
+import { NewsComponent } from './components/news/news.component';
+import { SearchComponent } from './components/search/search.component';
+import { Top10Component } from './components/top10/top10.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'top', component: Top10Component },
+  { path: 'search', component: SearchComponent },
+  { path: 'admin', component: AdminComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Blog-app/src/app/app.module.ts
+++ Blog-app/src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BodyComponent } from './components/body/body.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { NewsComponent } from './components/news/news.component';
+import { Top10Component } from './components/top10/top10.component';
+import { SearchComponent } from './components/search/search.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BodyComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    NewsComponent,
+    Top10Component,
+    SearchComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
